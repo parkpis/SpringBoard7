fe49865b05814e6889564cb2627366a3a0c0827f
parkpis <enffl0514@nate.com>
2025-06-11 13:03:15 +0900

    게시판 글쓰기 기능, 뷰페이지 연결 (board/regust.jsp) 연결

--- SpringBoard7/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoard7/src/main/java/com/itwillbs/controller/BoardController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 // @RequestMapping(value = "/board/*") //접근할수있는 주소 입력
 // => 실행하는 주소가 /board/~ 시작하는 모든 주소를
@@ -14,13 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/board/*") //접근할수있는 주소 입력
 public class BoardController {
 
-	//mylog
+	//mylog //??
 	private static final Logger logger = 
 			LoggerFactory.getLogger(BoardController.class);
 	
 	// 기능을 정의
-	
-	
+	// ?? void / String 차이??
+	// 글쓰기 (정보입력) / GET
+	// http://localhost:8088/controller/board/register (x) 톰캣으로 설정 변경 후 도메인 변경
+	// http://localhost:8088/board/register (o) 
+	@RequestMapping(value = "/regist", method = RequestMethod.GET)
+	public void boardRegistGET() {
+		
+		logger.info(" boardRegistGET() 실행 ");
+		logger.info(" /views/board/regist.jsp 페이지로 이동");
+		
+	}
+	// 글쓰기 (정보처리) / POST
 	
 	
 	
