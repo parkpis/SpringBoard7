7be3a03500082a5a202f81b946b058620a633d56
parkpis <enffl0514@nate.com>
2025-06-12 16:23:32 +0900

    BoardDAO / Impl 생성완료, BoardMapper에 해당 SQL 작성 완료(테스트 완료)

--- SpringBoard7/src/main/java/com/itwillbs/service/BoardServiseImpl.java
+++ SpringBoard7/src/main/java/com/itwillbs/service/BoardServiseImpl.java
@@ -1,8 +1,13 @@
 package com.itwillbs.service;
 
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.itwillbs.domain.BoardVO;
+import com.itwillbs.persistence.BoardDAO;
 
 /**
  * BoardServiseImpl : BoardService 인터페이스를 구현(상속)해서 만든 객체
@@ -10,12 +15,25 @@ import com.itwillbs.domain.BoardVO;
  * 객체를 직접 구현X => 주입해서 사용 ( 약한결합의 객체 관계일때 )
  * @Service 객체를 root-context.xml에서 불러올때 서비스 객체로 인식
  * 
+ * 컨트롤러의 요청을 받아서 DAO메서드를 호출
+ * 
+ * 
  */
 
 
 @Service
 public class BoardServiseImpl implements BoardServise{
 
+	// mylog
+	private static final Logger logger = 
+			LoggerFactory.getLogger(BoardServiseImpl.class);
+	
+	// BoardDAO 객체 필요 => 객체를 주입받아서 사용
+	@Inject
+	private BoardDAO bdao;
+	
+	
+	
 	// 오버라이딩 메서드 구현
 	@Override
 	public void boardRegist(BoardVO vo) throws Exception {
